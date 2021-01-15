c39a21b48efb244730fa28f9705a6fd7bd264f85
Mochammad Ifan Rizky Kurniawan <62989237+IfanRizky@users.noreply.github.com>
2021-01-15 09:48:24 +0700

    Add project files.

---
+++ ServerChatApp_085_IfanRizky/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_085;
+
+namespace ServerChatApp_085_IfanRizky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server ON, Ready to Use!");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
